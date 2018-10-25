7a85ab74ba0ae8ddd66eaf3e2abb6d7b26ee7122
Johann Knechtel <johann@nyu.edu>
2018-10-25 18:47:14 +0400

    added helper scripts to generate SLURM batch scripts for dalma

---
+++ sample_files/generate_randomize_netlist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for file in `ls input/`
+do
+	name=${file%.v}
+	script="randomize_"$name".sh"
+
+	echo "Generate $script ..."
+
+	cp randomize_netlist.sh.template $script
+
+	sed_string="s,netlist=TODO,netlist=input/$file,g"
+	sed -i "$sed_string" $script
+
+	sed_string="s,SBATCH -o TODO,SBATCH -o "$name".log,g"
+	sed -i "$sed_string" $script
+
+	sed_string="s,SBATCH -e TODO,SBATCH -o "$name".errors,g"
+	sed -i "$sed_string" $script
+done
